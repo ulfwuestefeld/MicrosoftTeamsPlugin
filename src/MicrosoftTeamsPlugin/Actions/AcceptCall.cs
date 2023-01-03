cb500777a12a4c34771c5917d35250a88d2bb4ed
Ulf Wüstefeld <ulf@wuesti.eu>
2023-01-03 13:20:45 +0100

    Added accept call and toggle cam

---
+++ src/MicrosoftTeamsPlugin/Actions/AcceptCall.cs
@@ -0,0 +1,39 @@
+﻿namespace Loupedeck.MicrosoftTeamsPlugin.Actions
+{
+    using System;
+
+    public class AcceptCall : PluginDynamicCommand
+    {
+        //private Boolean _toggleState = false;
+
+        private readonly String _image0ResourcePath;
+        //private readonly String _image1ResourcePath;
+
+        public AcceptCall() : base(displayName: "Accept call", description: "Accepts a call", groupName: "Appointments, calls and calender")
+        {
+            this._image0ResourcePath = EmbeddedResources.FindFile("AcceptCall0.png");
+            //this._image1ResourcePath = EmbeddedResources.FindFile("AcceptCall1.png");
+        }
+        protected override BitmapImage GetCommandImage(String actionParameter, PluginImageSize imageSize)
+        {
+            /*return !this._toggleState
+                ? EmbeddedResources.ReadImage(this._image0ResourcePath)
+                : EmbeddedResources.ReadImage(this._image1ResourcePath);*/
+            return EmbeddedResources.ReadImage(this._image0ResourcePath);
+        }
+        protected override void RunCommand(String actionParameter)
+        {
+            /*if (!this._toggleState)
+            {
+                this.Plugin.ClientApplication.SendKeyboardShortcut(VirtualKeyCode.KeyH);
+            }
+            else
+            {
+                this.Plugin.ClientApplication.SendKeyboardShortcut(VirtualKeyCode.KeyI);
+            }
+            this._toggleState = !this._toggleState;*/
+            this.Plugin.ClientApplication.SendKeyboardShortcut(VirtualKeyCode.KeyS, ModifierKey.Control | ModifierKey.Shift);
+            //this.ActionImageChanged();
+        }
+    }
+}
---
+++ src/MicrosoftTeamsPlugin/Actions/ToggleCam.cs
@@ -0,0 +1,30 @@
+﻿namespace Loupedeck.MicrosoftTeamsPlugin.Actions
+{
+    using System;
+
+    public class ToggleCam : PluginDynamicCommand
+    {
+        private Boolean _toggleState = false;
+
+        private readonly String _image0ResourcePath;
+        private readonly String _image1ResourcePath;
+
+        public ToggleCam() : base(displayName: "Raise hand", description: "Raises your hand", groupName: "Appointments, calls and calender")
+        {
+            this._image0ResourcePath = EmbeddedResources.FindFile("ToggleCam0.png");
+            this._image1ResourcePath = EmbeddedResources.FindFile("ToggleCam1.png");
+        }
+        protected override BitmapImage GetCommandImage(String actionParameter, PluginImageSize imageSize)
+        {
+            return !this._toggleState
+                ? EmbeddedResources.ReadImage(this._image0ResourcePath)
+                : EmbeddedResources.ReadImage(this._image1ResourcePath);
+        }
+        protected override void RunCommand(String actionParameter)
+        {
+            this._toggleState = !this._toggleState;
+            this.Plugin.ClientApplication.SendKeyboardShortcut(VirtualKeyCode.KeyO, ModifierKey.Control | ModifierKey.Shift);
+            this.ActionImageChanged();
+        }
+    }
+}
